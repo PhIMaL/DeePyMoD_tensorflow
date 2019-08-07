8852efd24f19c688dc61375cab83dc33fd02e1db
Remy <kusters.remy@gmail.com>
2019-08-07 10:11:56 +0200

    New update

--- src/deepymod/.ipynb_checkpoints/DeepMoD-checkpoint.py
+++ src/deepymod/.ipynb_checkpoints/DeepMoD-checkpoint.py
@@ -51,9 +51,8 @@ def DeepMoD(data, target, config, library_function, library_config, train_opts, 
 
 def thresholding(vector, mode, treshold=0.0):
     if mode == 'auto':
-        upper_lim, lower_lim = np.median(vector)+np.std(vector), np.median(vector) - np.std(vector)
+        upper_lim, lower_lim = np.median(vector)+0.05, np.median(vector) - 0.05
         sparsity_mask = (vector <= upper_lim) & (vector >= lower_lim)
-    else:
-        sparsity_mask = np.abs(vector) < treshold
+    
 
     return ~sparsity_mask
